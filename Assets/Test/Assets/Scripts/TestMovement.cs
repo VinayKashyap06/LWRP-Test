eec5712201da26a86177bc202df2d9930f6cbca4
Vinay Kashyap <vinaykashyap57@gmail.com>
2019-04-30 15:44:45 +0530

    Test scenes and assets

---
+++ Assets/Test/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Testing
+{
+    public class TestMovement : MonoBehaviour
+    {        
+
+        void Update()
+        {
+            transform.position += new Vector3(0, 0, Input.GetAxis("Vertical"))* Time.deltaTime*5f;
+            transform.eulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        }
+    }
+}
